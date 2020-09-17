808dd76bf644e3d7041b0732b32d6dad0ff553bd
Christian Aistleitner <christian@quelltextlich.at>
2020-09-17 12:10:55 +0200

    Add script to reindex all dynamic content

---
+++ reindex.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+import logging
+
+from common import get_dynamic_directory
+from common import reindex_actors_for_scene
+
+logger = logging.getLogger(__name__)
+
+
+def reindex():
+    scenes_dir = os.path.join(get_dynamic_directory(), 'scenes')
+    if os.path.isdir(scenes_dir):
+        for scene in os.listdir(scenes_dir):
+            scene_dir = os.path.join(scenes_dir, scene)
+            if os.path.isdir(scene_dir):
+                reindex_actors_for_scene(scene)
+
+
+if __name__ == "__main__":
+    reindex()

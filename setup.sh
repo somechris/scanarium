e5db5b368f3bed3c3f53eed5f26d36c7ee4e9c93
Christian Aistleitner <christian@quelltextlich.at>
2020-09-18 15:27:16 +0200

    Add `setup.sh` to bring fetch phaser and bring config into place
    This script should help users to bootstrap Scanarium.

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+cd "$(dirname "$0")"
+
+PHASER_VERSION="3.11.0"
+PHASER_TARGET="frontend/phaser-${PHASER_VERSION}.js"
+CONF_TARGET="conf/scanarium.conf"
+
+step() {
+    local NAME="$1"
+    local TARGET="$2"
+    shift 2
+
+    echo
+    echo "#################################################################"
+    echo "Setting up $NAME at '$TARGET' ..."
+    echo
+
+    if [ ! -e "$TARGET" ]
+    then
+        echo "Running:" "$@"
+        "$@"
+    else
+        echo "(Skipping as '$TARGET' already exists)"
+    fi
+}
+
+
+step "phaser ${PHASER_VERSION}" "$PHASER_TARGET" curl --output "$PHASER_TARGET" "https://cdn.jsdelivr.net/npm/phaser@${PHASER_VERSION}/dist/phaser.js"
+step "example configuration" "$CONF_TARGET" cp "conf/scanarium.conf.example" "$CONF_TARGET"
+
+echo
+echo "#################################################################"
+echo
+echo "All done."
+echo
+echo "Now edit '$CONF_TARGET' to your liking and start Scanarium by running ./run-demo-server.sh"

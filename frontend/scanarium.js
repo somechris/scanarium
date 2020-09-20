386a13482f736b0b832c25b8df367c54f1827364
Christian Aistleitner <christian@quelltextlich.at>
2020-09-20 23:19:32 +0200

    frontend: Only add actor, if there are frequent updates
    If updates occur only infrequently (e.g.: tab is in the background),
    actors don't get enough frames to move off of the screen. Yet we added
    new actors at full speed. This lead to scenes being boastingly full
    with actors when coming back to a backgcound tab.
    
    To avoid such issues, we only add new actors, if the screen gets
    frequent updates (and actors can hence move off of the screen).
    
    Closes: #7

--- frontend/scanarium.js
+++ frontend/scanarium.js
@@ -65,7 +65,16 @@ var ScActorManager = {
         }
 
         if (time > this.nextSpawn) {
-            this.addActorRandom();
+            // In a foreground tab, delta is around 16ms (i.e.: 60 Hz). In a
+            // background tab, delta is often way above 10 seconds. So we'd
+            // spawn basically every update cycle, while not giving actors
+            // frames to move out of the screen. This would lead to a screen
+            // that's boastingly full with actors when coming back to a
+            // background tab. To avoid that, we only add ships, if the delay is
+            // short enough.
+            if (delta < 100) {
+                this.addActorRandom();
+            }
             this.nextSpawn = time + scanariumConfig['spawnPeriod']
         }
 

8c771e434f3746bcb49951a9940e88c3a0b407fb
Christian Aistleitner <christian@quelltextlich.at>
2020-10-02 10:52:20 +0200

    reindex: Also reindex scenes without actors
    This helps when bootstrapping a new scene, as it now becomes playable
    upon reindex, even if it does not have actors yet.
    This is useful for giving scene backgrounds a test drive before
    creating actors.

--- backend/reindex.py
+++ backend/reindex.py
@@ -6,6 +6,7 @@ import sys
 
 SCANARIUM_DIR_ABS = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, SCANARIUM_DIR_ABS)
+from common import SCENES_DIR_ABS
 from common import call_guarded
 from common import get_dynamic_directory
 from common import reindex_actors_for_scene
@@ -15,12 +16,13 @@ logger = logging.getLogger(__name__)
 
 
 def reindex():
-    scenes_dir = os.path.join(get_dynamic_directory(), 'scenes')
-    if os.path.isdir(scenes_dir):
-        for scene in os.listdir(scenes_dir):
-            scene_dir = os.path.join(scenes_dir, scene)
-            if os.path.isdir(scene_dir):
-                reindex_actors_for_scene(scene)
+    dynamic_scenes_dir = os.path.join(get_dynamic_directory(), 'scenes')
+    for scene in os.listdir(SCENES_DIR_ABS):
+        if os.path.isdir(os.path.join(SCENES_DIR_ABS, scene)):
+            dynamic_scene_dir = os.path.join(dynamic_scenes_dir, scene)
+            if not os.path.isdir(dynamic_scene_dir):
+                os.makedirs(dynamic_scene_dir)
+            reindex_actors_for_scene(scene)
 
 
 if __name__ == "__main__":

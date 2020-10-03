fb5ceb35afb329ef2528e7508f231f5e0b5599f0
Christian Aistleitner <christian@quelltextlich.at>
2020-10-03 12:50:47 +0200

    balloons: Add clouds

--- scenes/balloons/scene.js
+++ scenes/balloons/scene.js
@@ -1,37 +1,84 @@
 // Scene: balloons
 
 function scene_preload() {
+    game.load.image('Cloud-1', scene_dir + '/cloud1.png');
+    game.load.image('Cloud-2', scene_dir + '/cloud2.png');
+    game.load.image('Cloud-3', scene_dir + '/cloud3.png');
+}
+
+function spawnCloud() {
+    var flavor = tunnel(Math.floor(Math.random()*3) + 1, 1, 3)
+    return ScActorManager.addFullyLoadedActor('Cloud', flavor);
 }
 
 function scene_create() {
     Wind.init();
+    ScActorManager.registerActor('Cloud', Cloud);
+    ScActorManager.onActorDestroy(function(actor) {spawnCloud();}, 'Cloud');
+    var i;
+    for (i=0; i<scanariumConfig.height / 50; i++) {
+        var cloud = spawnCloud();
+        cloud.alpha = 1;
+        cloud.x = Math.random() * scanariumConfig.width;
+    }
 }
 
 function scene_update(time, delta) {
     Wind.update(time, delta);
 }
 
-class BaseBalloon extends Phaser.Physics.Arcade.Sprite {
-    constructor(actor, flavor, x, y) {
-        super(game, x, scanariumConfig.height + 2000, actor + '-' + flavor);
+class WindAffectedSprite extends Phaser.Physics.Arcade.Sprite {
+    constructor(x, y, actor, flavor, minScale, maxScale) {
+        super(game, x, y, actor + '-' + flavor);
         game.physics.world.enable(this);
         this.depth = Math.random();
-        var scale = scaleBetween(0.4, 1, this.depth);
+        var scale = scaleBetween(minScale, maxScale, this.depth);
         var width = this.width * scale;
         var height = this.height * scale;
-        this.y = scanariumConfig.height + height/2;
         this.setDisplaySize(width, height);
         this.setSize(width, height);
         this.destroyOffset = Math.sqrt(width * width + height * height) / 2;
         this.speedX = 0;
-        this.speedY = -20 * (1 + this.depth);
+        this.speedY = 0;
     }
 
     update(time, delta) {
         this.speedX = tunnel(this.speedX + Wind.getForce(this.y), -10, 10);
-        this.setAngle(this.speedX);
         this.setVelocityX(this.speedX);
         this.setVelocityY(this.speedY);
+    }
+}
+
+class Cloud extends WindAffectedSprite {
+    constructor(x, y, flavor) {
+        super(x, y, 'Cloud', flavor, 0.2, 0.8);
+        this.y = Math.random() * scanariumConfig.height;
+        this.x = Math.random() * scanariumConfig.width/2;
+        var force = Wind.getForce(this.y);
+        if (force <=0) {
+            this.x += scanariumConfig.width/2;
+        }
+        this.speedX = tunnel(force * 120, -10, 10);
+        this.destroyOffset = this.displayWidth / 2;
+        this.alpha = 0;
+    }
+
+    update(time, delta) {
+        super.update(time, delta);
+        this.alpha += 0.01;
+    }
+}
+
+class BaseBalloon extends WindAffectedSprite {
+    constructor(actor, flavor, x, y) {
+        super(x, y, actor, flavor, 0.4, 1);
+        this.y = scanariumConfig.height + this.displayHeight/2;
+        this.speedY = -20 * (1 + this.depth);
+    }
+
+    update(time, delta) {
+        super.update(time, delta);
+        this.setAngle(this.speedX);
     }
 }
 

e8e50843e457d0dc7029563586ff8f81c3ad9fc6
Christian Aistleitner <christian@quelltextlich.at>
2020-10-30 14:46:49 +0100

    Scan for dedicated rect instead of paper contour
    The paper contour is not too reliable, as it depends on the external
    factors. One such factor is the background, which might have features
    that let the paper sheet appear to have outward finger-like
    extensions. Another factor is the printer paper feeding
    mechanism. Some paper feeding mechanisms allow for small random
    rotations, which make it hard to reliably correlate the actor drawing
    within the scanned paper contour. These and further factors are in the
    way of a reliable and spot-on detection.
    
    To improve detection quality, we add a 2mm rectangle to the border of
    actor images and detect this (instead of the paper edges). As this
    rectangle is always surrounded by the white paper, so this method is
    no longer as dependent of the background of the scanning plane. And
    this method also fixes the relative position between the detected
    contour and the relevant image content.
    
    With this change we arrive at reliable, spot-on detection even on old
    640x480 cameras.

--- backend/scan.py
+++ backend/scan.py
@@ -5,10 +5,12 @@ import os
 import re
 import time
 import sys
+import locale
 
 import cv2
 import numpy as np
 from pyzbar import pyzbar
+import xml.etree.ElementTree as ET
 
 SCANARIUM_DIR_ABS = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, SCANARIUM_DIR_ABS)
@@ -35,18 +37,38 @@ def scale_image(scanarium, image):
     return (scaled_image, scale_factor)
 
 
-def find_sheet_points(image):
+def find_rect_points(image, decreasingArea=True, required_points=[]):
+    imageArea = image.shape[0] * image.shape[1]
+    contour_min_area = imageArea / 25
+
     cannied = cv2.Canny(image, 30, 400)
+    # When looking for contours that contain some QR code, RETR_LIST (below)
+    # might not be most efficient, RETR_TREE might allow to optimize. But
+    # RETR_LIST is simpler to use and quick enough for now.
+    # todo: See if RETR_TREE performs better here.
     contours, _ = cv2.findContours(cannied, cv2.RETR_LIST,
                                    cv2.CHAIN_APPROX_NONE)
 
     approx = None
-    for contour in sorted(contours, key=cv2.contourArea, reverse=True):
+    for contour in sorted(contours, key=cv2.contourArea,
+                          reverse=decreasingArea):
+        if cv2.contourArea(contour) < contour_min_area:
+            # Contour too small, so we skip this contour
+            continue
+
         peri = cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
 
         if len(approx) == 4:
             # 4 points ... that looks should be turned into a rectangle
+
+            if any(cv2.pointPolygonTest(approx, point, False) < 0
+                   for point in required_points):
+                # A required point is outside, so we skip this contour
+                continue
+
+            # The contour is big enough, looks like a rect, and contains all
+            # required points. That's the contour to continue with.
             break
 
     if approx is None:
@@ -61,7 +83,7 @@ def distance(pointA, pointB):
     return np.linalg.norm([pointA - pointB])
 
 
-def rectify(image, points):
+def rectify_by_rect_points(image, points):
     # The following heuristics of classifying the 4 points is based on the
     # assumption that the rectangle is not distorted too much. So if the
     # camera angle is skew, it will fail.
@@ -84,10 +106,49 @@ def rectify(image, points):
     return cv2.warpPerspective(image, M, (d_w, d_h))
 
 
-def turn_landscape(image):
-    if image.shape[0] > image.shape[1]:
-        image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+def rectify(scanarium, image, decreasingArea=True, required_points=[]):
+    # If the picture is too big (E.g.: from a proper photo camera), edge
+    # detection won't work reliably, as the sheet's contour will exhibit too
+    # much detail and would get broken down into more than 4 segments. So we
+    # scale too big images down. Note though that the scaled image is only
+    # used for edge detection. Rectification happens on the original picture.
+    (scaled_image, scale_factor) = scale_image(scanarium, image)
+    scaled_points = [(int(point[0] * scale_factor),
+                      int(point[1] * scale_factor)
+                      ) for point in required_points]
+    rect_points = find_rect_points(scaled_image, decreasingArea, scaled_points)
+
+    # todo: Check if subpixel corner detection improves the overall result
+
+    # Now rectifying using the original (!) image.
+    image = rectify_by_rect_points(image, rect_points / scale_factor)
     return image
+
+
+def rectify_to_qr_parent_rect(scanarium, image, qr_rect):
+    def qr_rect_point(x_factor, y_factor):
+        return (qr_rect.left + x_factor * qr_rect.width,
+                qr_rect.top + y_factor * qr_rect.height)
+
+    inset_factor = 0.2
+    top_left_inset = qr_rect_point(inset_factor, inset_factor)
+    top_right_inset = qr_rect_point(1. - inset_factor, inset_factor)
+    bottom_left_inset = qr_rect_point(inset_factor, 1. - inset_factor)
+    bottom_right_inset = qr_rect_point(1. - inset_factor, 1. - inset_factor)
+
+    required_points = [
+        top_left_inset,
+        top_right_inset,
+        bottom_left_inset,
+        bottom_right_inset,
+    ]
+
+    return rectify(scanarium, image, decreasingArea=False,
+                   required_points=required_points)
+
+
+def rectify_to_biggest_rect(scanarium, image):
+    return rectify(scanarium, image, decreasingArea=True)
 
 
 def scale_to_paper_size(scanarium, image):
@@ -115,13 +176,121 @@ def extract_qr(image):
     return (rect, scene, actor)
 
 
-def orient_image(image, left_coordinate):
-    if left_coordinate > image.shape[1] / 2:
+def orient_image(image):
+    if image.shape[0] > image.shape[1]:
+        image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+
+    (qr_rect, _, _) = extract_qr(image)
+    if qr_rect.left + qr_rect.width / 2 > image.shape[1] / 2:
         # QR Code is not on the left half of the picture. As it's landscape
         # (see above), the qr code is in the top-right corner and we need to
         # rotate 180 degrees.
         image = cv2.rotate(image, cv2.ROTATE_180)
+
     return image
+
+
+def get_svg_document_to_user_dpi_factors(scanarium, tree):
+    root = tree.getroot()
+
+    def to_inch(name, default):
+        raw = root.attrib.get(name, default)
+        ret = 0
+        if raw.endswith('mm'):
+            ret = float(raw[:-2]) / 25.4
+        elif raw.endswitch('in'):
+            ret = float(raw[:-2])
+        else:
+            ret = float(raw)
+        return ret
+
+    paper_width = scanarium.get_config('scan', 'paper_width', 'int')
+    paper_height = scanarium.get_config('scan', 'paper_height', 'int')
+    width = to_inch('width', '%dmm' % (paper_width))
+    height = to_inch('height', '%dmm' % (paper_height))
+    view_box = root.attrib.get('viewBox',
+                               '0 0 %d %d' % (paper_width, paper_height)
+                               ).split(' ')
+    view_box_width = float(view_box[2]) - float(view_box[0])
+    view_box_height = float(view_box[3]) - float(view_box[1])
+    document_dpi_x = view_box_width / width
+    document_dpi_y = view_box_height / height
+    inkscape_dpi = scanarium.get_config('programs', 'inkscape_dpi', 'int')
+    document_to_user_factor_x = inkscape_dpi / document_dpi_x
+    document_to_user_factor_y = inkscape_dpi / document_dpi_y
+
+    return (document_to_user_factor_x, document_to_user_factor_y)
+
+
+def get_svg_contour_rect_stroke_width(tree):
+    stroke_width = 0
+    try:
+        path = './/{http://www.w3.org/2000/svg}rect[@id="contour"]'
+        contour = tree.find(path)
+        for setting in contour.attrib.get('style', '').split(';'):
+            k_raw, v_raw = setting.split(':', 1)
+            if k_raw.strip() == 'stroke-width':
+                stroke_width = float(v_raw.strip())
+    except Exception:
+        logging.exception('Failed to parse "stroke-width"')
+
+    return stroke_width
+
+
+def get_svg_contour_rect_area(scanarium, svg_path):
+    # We want to determine the white inner area of the rect with id
+    # `contour` in user coordinates. Ideally, Inkscape would allow to access
+    # that directly. However, its `--query*` arguments only allow to get the
+    # rect's outer dimensions. But we need the rect's inner dimensions
+    # (without border). So we have to compute that manually.
+    # This is a 5 step process:
+    # 1. Determine the factor from document units to user units
+    # 2. Extract the stroke width from the document
+    # 3. Scale it accordingly
+    # 4. Determine contour rect position and dimension
+    # 5. Compute contour's inner rect
+
+    # 1. Determine the factor from document units to user units
+    tree = ET.parse(svg_path)
+    document_to_user_factor_x, document_to_user_factor_y = \
+        get_svg_document_to_user_dpi_factors(scanarium, tree)
+
+    # 2. Extract the stroke width from the document
+    stroke_width = get_svg_contour_rect_stroke_width(tree)
+
+    # 3. Scale it accordingly
+    user_stroke_width_x = stroke_width * document_to_user_factor_x
+    user_stroke_width_y = stroke_width * document_to_user_factor_y
+
+    # 4. Determine contour rect position and dimension
+    inkscape_dpi = scanarium.get_config('programs', 'inkscape_dpi', 'float')
+    paper_width = scanarium.get_config('scan', 'paper_width', 'int')
+    paper_height = scanarium.get_config('scan', 'paper_height', 'int')
+
+    x = 0
+    y = 0
+    width = inkscape_dpi / 25.4 * paper_width
+    height = inkscape_dpi / 25.4 * paper_height
+
+    command = [
+        scanarium.get_config('programs', 'inkscape'),
+        '--query-all',
+        svg_path,
+    ]
+    element_sizes = scanarium.run(command).split('\n')
+    for element_size in element_sizes:
+        if element_size.startswith('contour,'):
+            x, y, width, height = map(float, element_size.split(',')[1:])
+
+    # 5. Compute contour's inner rect
+    # Since we want Python 3.6 compatibility, we cannot use %n and need to
+    # resort to locale.format_string with %f.
+    return locale.format_string('%f:%f:%f:%f', (
+        (x + user_stroke_width_x),
+        (y + user_stroke_width_y),
+        (x + width - user_stroke_width_x),
+        (y + height - user_stroke_width_y),
+    ))
 
 
 def generate_mask(scanarium, mask_path):
@@ -133,11 +302,12 @@ def generate_mask(scanarium, mask_path):
 
     if not os.path.isfile(mask_path) \
             or os.stat(source_path).st_mtime > os.stat(mask_path).st_mtime:
+        contour_area = get_svg_contour_rect_area(scanarium, source_path)
         command = [
             scanarium.get_config('programs', 'inkscape'),
             '--export-id=Mask',
             '--export-id-only',
-            '--export-area-page',
+            f'--export-area={contour_area}',
             '--export-background=black',
             '--export-png=%s' % (mask_path),
             source_path,
@@ -216,27 +386,39 @@ def save_image(scanarium, image, scene, actor):
 def scan_actor_image(scanarium):
     image = scanarium.get_image()
 
-    scanarium.debug_show_image('scanned', image)
+    qr_rect = None
+    scene = None
+    actor = None
+    iteration = 1
+    while qr_rect is None:
+        if iteration > 3:
+            raise ScanariumError(
+                'SE_SCAN_IMAGE_TOO_MANY_ITERATIONS',
+                'Taken too many extraction tries from scanned image')
 
-    # If the picture is too big (E.g.: from a proper photo camera), edge
-    # detection won't work reliably, as the sheet's contour will exhibit too
-    # much detail and would get broken down into more than 4 segments. So we
-    # scale too big images down. Note though that the scaled image is only
-    # used for edge detection. Rectification happens on the original picture.
-    (scaled_image, scale_factor) = scale_image(scanarium, image)
-    points = find_sheet_points(scaled_image)
+        if image.shape[1] < 150:
+            # The image that we're homing in on is really small. It's unlikely
+            # to be a proper A4 image, but rather the camera did not detect a
+            # proper sheet rect and we're homing in on an (unrelated) small
+            # rectangular part of the image. So we abort.
+            raise ScanariumError('SE_SCAN_IMAGE_GREW_TOO_SMALL',
+                                 'Failed to identify sheet on scanned image')
+        try:
+            (qr_rect, scene, actor) = extract_qr(image)
+            image = rectify_to_qr_parent_rect(scanarium, image, qr_rect)
+        except ScanariumError as e:
+            if e.code == 'SE_SCAN_NO_QR_CODE':
+                # QR code could not get scanned. Probably, because the image
+                # is too skew. We try to rectify on the images biggest rect
+                # (probably the paper sheet). This should undistort the QR
+                # code to be scanable in the next round.
+                image = rectify_to_biggest_rect(scanarium, image)
+            else:
+                raise e
 
-    # Now rectifying using the original (!) image.
-    image = rectify(image, points / scale_factor)
+        iteration += 1
 
-    # Now normalizing the rectified image
-    image = turn_landscape(image)
-    image = scale_to_paper_size(scanarium, image)
-
-    scanarium.debug_show_image('before QR extraction', image)
-    (qr_rect, scene, actor) = extract_qr(image)
-
-    image = orient_image(image, qr_rect.left)
+    image = orient_image(image)
     image = mask(scanarium, image, scene, actor)
     image = crop(image)
     image = balance(scanarium, image)

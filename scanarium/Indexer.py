a285e0623a228056ffc8237cba2aed3f68b27f78
Christian Aistleitner <christian@quelltextlich.at>
2020-11-25 10:49:26 +0100

    Split too long lines

--- scanarium/Indexer.py
+++ scanarium/Indexer.py
@@ -23,7 +23,8 @@ class Indexer(object):
             for actor in os.listdir(actors_dir):
                 actor_dir = os.path.join(actors_dir, actor)
                 if os.path.isdir(actor_dir):
-                    logging.debug(f'Reindexing scene "{scene}" actor "{actor}" ...')
+                    logging.debug(
+                        f'Reindexing scene "{scene}" actor "{actor}" ...')
                     flavor_files = []
                     for flavor in os.listdir(actor_dir):
                         flavor_file = os.path.join(actor_dir, flavor)

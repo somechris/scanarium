4430f2a8e79d5554a7fb4b1f78702124a937dead
Christian Aistleitner <christian@quelltextlich.at>
2021-02-01 09:21:42 +0100

    frontend: Update pixelFactor upon resizes

--- frontend/scanarium.js
+++ frontend/scanarium.js
@@ -152,7 +152,10 @@ var dynamicConfigMethod = 'GET';
 
 var degToRadian = 2 * Math.PI / 360;
 
-var pixelFactor = Math.max(window.innerWidth, window.innerHeight) * window.devicePixelRatio / 1920;
+var pixelFactor = 1;
+LayoutManager.register(function(width, height) {
+  pixelFactor = Math.max(width, height) * window.devicePixelRatio / 1920;
+});
 
 function scaleBetween(min, max, scale) {
     return (max - min) * scale + min;

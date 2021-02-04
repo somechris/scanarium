41f219a7804bbb4236cff105742cfe7eb90783fc
Christian Aistleitner <christian@quelltextlich.at>
2021-02-04 20:16:56 +0100

    space: Add astronaut actor

---
+++ scenes/space/actors/Astronaut/Astronaut.js
@@ -0,0 +1,15 @@
+class Astronaut extends SpaceshipBase {
+    constructor(x, y, flavor) {
+        var lengthMin = 50;
+        var lengthMax = 200;
+
+        super('Astronaut', flavor, x, y, 90, lengthMin, lengthMax);
+
+        var thrustScale = scaleBetween(0.10, 0.30, this.base_scale);
+
+        this.addThruster(-0.4, -0.1, -80, thrustScale, 0.3, 0.9); // Left
+        this.addThruster(0.4, -0.1, -100, thrustScale, -0.3, 0.9); // Right
+    }
+}
+
+ScActorManager.registerActor('Astronaut', Astronaut);

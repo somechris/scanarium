e1d771801b53929c49eddfda98a0e1a389697e24
Christian Aistleitner <christian@quelltextlich.at>
2021-02-06 12:17:27 +0100

    highway: Let vehicles drive in correct direction on each lane

--- scenes/highway/scene.js
+++ scenes/highway/scene.js
@@ -2,20 +2,24 @@
 
 var lanes=[];
 
-function add_lane(y) {
-  lanes.push({y: y});
+function add_lane(y, leftToRight) {
+  lanes.push({
+    y: y,
+    leftToRight: leftToRight,
+    speedFactor: (leftToRight ? 1 : -1),
+  });
 }
 
 function scene_preload() {
 }
 
 function scene_create() {
-  add_lane(0.255);
-  add_lane(0.300);
-  add_lane(0.460);
-  add_lane(0.560);
-  add_lane(0.780);
-  add_lane(0.920);
+  add_lane(0.255, false);
+  add_lane(0.300, true);
+  add_lane(0.460, false);
+  add_lane(0.560, true);
+  add_lane(0.780, false);
+  add_lane(0.920, true);
 }
 
 function scene_update(time, delta) {
@@ -24,14 +28,15 @@ function scene_update(time, delta) {
 class Vehicle extends Phaser.GameObjects.Container {
     constructor(actor, flavor, x, y, width) {
         var lane = lanes[tunnel(Math.floor(Math.random()*lanes.length), 0, lanes.length-1)];
-        var x = scanariumConfig.width;
+        var x = lane.leftToRight ? 0 : scanariumConfig.width;
         var y = lane.y * scanariumConfig.height;
         super(game, x, y);
 
         var body = game.add.image(0, 0, actor + '-' + flavor);
         var width = width * refToScreen;
         var height = body.height / body.width * width;
-        body.setOrigin(0, 1);
+        body.setFlipX(lane.leftToRight);
+        body.setOrigin((lane.leftToRight ? 1 : 0), 1);
         body.setSize(width, height);
         body.setDisplaySize(width, height);
         this.destroyOffset = width;
@@ -40,7 +45,7 @@ class Vehicle extends Phaser.GameObjects.Container {
 
         game.physics.world.enable(this);
 
-        var speed = -(Math.random()+1) * 100 * refToScreen;
+        var speed = (Math.random()+1) * 100 * lane.speedFactor * refToScreen;
         this.body.setVelocityX(speed);
     }
 

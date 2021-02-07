26e5ef68beeaf72a01871252541e92803a2babcf
Christian Aistleitner <christian@quelltextlich.at>
2021-02-07 09:08:00 +0100

    highway: Make vehicle wheels turn

--- scenes/highway/actors/OldFamilyCar/OldFamilyCar.js
+++ scenes/highway/actors/OldFamilyCar/OldFamilyCar.js
@@ -1,6 +1,10 @@
 class OldFamilyCar extends Vehicle {
     constructor(x, y, flavor) {
-        super('OldFamilyCar', flavor, x, y, 500);
+        const tires = [
+          {x1: 46, x2: 104, y1: 92, y2: 150, w: 398, h: 151, clearance: 1},
+          {x1: 285, x2: 343, y1: 92, y2: 150, w: 398, h: 151, clearance: 1},
+        ]
+        super('OldFamilyCar', flavor, x, y, 500, tires);
     }
 }
 
--- scenes/highway/scene.js
+++ scenes/highway/scene.js
@@ -26,17 +26,29 @@ function scene_create() {
 function scene_update(time, delta) {
 }
 
+class Tire extends Phaser.Physics.Arcade.Sprite {
+  constructor(x, y, image_name) {
+    super(game, x, y, image_name);
+
+    game.physics.world.enableBody(this);
+    game.sys.updateList.add(this);
+  }
+}
+
 class Vehicle extends Phaser.GameObjects.Container {
-    constructor(actor, flavor, x, y, width) {
+    constructor(actor, flavor, x, y, width, tires) {
         var lane = lanes[tunnel(Math.floor(Math.random()*lanes.length), 0, lanes.length-1)];
         var x = lane.leftToRight ? 0 : scanariumConfig.width;
         var y = lane.y * scanariumConfig.height;
         super(game, x, y);
 
         this.setDepth(lane.scale*100);
-        var body = game.add.image(0, 0, actor + '-' + flavor);
+        var image_name = actor + '-' + flavor;
+        this.createTextures(image_name, tires);
+        var body = game.add.image(0, 0, image_name + '-body');
         var width = width * lane.scale * refToScreen;
-        var height = body.height / body.width * width;
+        var base_scale = width / body.width;
+        var height = body.height * base_scale;
         body.setFlipX(lane.leftToRight);
         body.setOrigin((lane.leftToRight ? 1 : 0), 1);
         body.setSize(width, height);
@@ -44,11 +56,88 @@ class Vehicle extends Phaser.GameObjects.Container {
         this.destroyOffset = width;
         this.add([body]);
         this.vehicle_body = body;
+        this.tires = [];
 
         game.physics.world.enable(this);
 
         var speed = (Math.random()+1) * 100 * lane.speedFactor * refToScreen;
         this.body.setVelocityX(speed);
+
+        var that = this;
+        tires.forEach((tire, i) => {
+          const cx = (tire.x1 + tire.x2) / 2 * base_scale * (lane.leftToRight ? -1 : 1);
+          const cy = (tire.y1 + tire.y2) / 2 * base_scale - body.height;
+          var tire = new Tire(cx, cy, image_name + '-tire-' + i);
+          tire.setFlipX(lane.leftToRight);
+          tire.r = tire.width * base_scale / 2;
+          tire.setSize(tire.width * base_scale, tire.height * base_scale);
+          tire.setDisplaySize(tire.width * base_scale, tire.height * base_scale);
+          tire.setAngularVelocity(that.body.velocity.x * 360 / 2 / Math.PI / tire.r);
+          that.add(tire);
+          that.tires.push(tire);
+        });
+    }
+
+    createTextures(image_name, tires) {
+      if (!game.textures.exists(image_name + '-body')) {
+        this.createTexturesForce(image_name, tires);
+      }
+    }
+
+    createTexturesForce(image_name, tires) {
+      const full_texture = game.textures.get(image_name);
+      const full_texture_source_index = 0;
+      const full_source = full_texture.source[full_texture_source_index];
+      const full_width = full_source.width;
+      const full_height = full_source.height;
+
+      var body = game.make.renderTexture({
+        width: full_width,
+        height: full_height,
+      }, false);
+
+      body.draw(image_name);
+
+      tires.forEach((spec, i) => {
+        const name = 'tire-' + i;
+
+        const x1 = (spec.x1 - spec.clearance) / spec.w * full_width;
+        const y1 = (spec.y1 - spec.clearance) / spec.h * full_height;
+        const x2 = (spec.x2 + spec.clearance) / spec.w * full_width;
+        const y2 = (spec.y2 + spec.clearance) / spec.h * full_height;
+        const cx = (spec.x1 + spec.x2) / 2 / spec.w * full_width;
+        const cy = (spec.y1 + spec.y2) / 2 / spec.h * full_height;
+        const r = x2 - cx + 1;
+
+        var frameOriginal = full_texture.add(name, full_texture_source_index, x1, y1, x2 - x1 + 1, y2 - y1 + 1);
+        var tire = game.make.renderTexture({
+          width: x2 - x1 + 1,
+          height: y2 - y1 + 1,
+        }, false);
+
+        tire.drawFrame(image_name, name);
+
+        var tireEraser = game.make.renderTexture({
+          width: x2 - x1 + 1,
+          height: y2 - y1 + 1,
+        }, false);
+        tireEraser.fill(0xffffff, 1);
+
+        var tireEraserEraser = game.make.graphics();
+        tireEraserEraser.fillStyle(0xffffff, 1);
+        tireEraserEraser.fillCircle(cx - x1, cy - y1, r);
+        tireEraser.erase(tireEraserEraser);
+
+        tire.erase(tireEraser);
+        tire.saveTexture(image_name + '-' + name);
+
+        const bodyEraser = game.make.graphics();
+        bodyEraser.fillStyle(0xffffff, 1);
+        bodyEraser.fillCircle(cx, cy, r);
+        body.erase(bodyEraser);
+      })
+
+      body.saveTexture(image_name + '-body');
     }
 
     update(time, delta) {

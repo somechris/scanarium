2d3d5adf9a5a065f5f85c58277ec25c4cf08f571
Christian Aistleitner <christian@quelltextlich.at>
2021-02-13 16:43:38 +0100

    space: Add Star actor

---
+++ scenes/space/actors/Star/Star.js
@@ -0,0 +1,48 @@
+class Star extends SpaceshipBase {
+  constructor(x, y, flavor) {
+    var lengthMin = 50;
+    var lengthMax = 200;
+
+    super('Star', flavor, x, y, 90, lengthMin, lengthMax);
+
+    this.body.setVelocityX(0);
+    this.body.setVelocityY(0);
+    sendToBack(this);
+
+    this.addTimeline();
+  }
+
+  addTimeline() {
+    var tweens = [];
+
+    this.alpha = 0;
+
+    // Gradually become visible
+    tweens.push({
+      alpha: 1,
+      duration: randomBetween(1000, 3000),
+      hold: randomBetween(10000, 20000),
+    });
+
+    // Gradually vanish again
+    tweens.push({
+      alpha: 0,
+      duration: randomBetween(1000, 4000),
+    });
+
+    // Move beyond destroyOffset to get cleaned up
+    tweens.push({
+      x: -this.destroyOffset - 10,
+      y: -this.destroyOffset - 10,
+      duration: randomBetween(1000, 4000),
+    });
+
+    this.timeline = game.tweens.timeline({
+      targets: this,
+      ease: 'Cubic.easeInOut',
+      tweens: tweens,
+    });
+  }
+}
+
+ScActorManager.registerActor('Star', Star);

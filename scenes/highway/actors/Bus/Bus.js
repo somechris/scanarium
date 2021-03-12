349257e40211496a10b1de8c04e620c4e0294441
Christian Aistleitner <christian@quelltextlich.at>
2021-03-12 15:36:42 +0100

    highway: Add bus actor

---
+++ scenes/highway/actors/Bus/Bus.js
@@ -0,0 +1,19 @@
+class Bus extends Vehicle {
+    constructor(x, y, flavor) {
+        const initialMinSpeed = 400;
+        const initialMaxSpeed = 750;
+        const width = 1000;
+        const tires = [
+          {x1: 73, x2: 116, y1: 116, y2: 159, w: 460, h: 160},
+          {x1: 347, x2: 390, y1: 116, y2: 159, w: 460, h: 160},
+        ];
+        const undercarriage = [
+          {points: [[60, 144], [410, 144], [410, 100], [60, 100]], w: 460, h: 160},
+          ];
+        const angularShake = 0.1;
+        const yShake = 1.5;
+        super('Bus', flavor, x, y, initialMinSpeed, initialMaxSpeed, width, tires, undercarriage, angularShake, yShake);
+    }
+}
+
+ScActorManager.registerActor('Bus', Bus);

0755648f18f705d668308ec53583ea501d75516b
Christian Aistleitner <christian@quelltextlich.at>
2021-03-27 11:55:53 +0100

    LocalizerFactor: Cache created localizers
    This makes requesting a localizer for the same language again and again
    cheaper and helps us with regenerating PDFs in different languages in
    follow-up commits.

--- scanarium/LocalizerFactory.py
+++ scanarium/LocalizerFactory.py
@@ -11,15 +11,21 @@ class LocalizerFactory(object):
     def __init__(self, localization_dir_abs):
         super(LocalizerFactory, self).__init__()
         self._localization_dir_abs = localization_dir_abs
+        self._instances = {}
 
     def get_localizer(self, language=None):
-        ret = {}
+        try:
+            ret = self._instances[language]
+        except KeyError:
+            translations = {}
 
-        if language is not None:
-            json_file_abs = os.path.join(
-                self._localization_dir_abs, f'{language}.json')
-            if os.path.isfile(json_file_abs):
-                with open(json_file_abs, 'r') as file:
-                    ret = json.load(file)
+            if language is not None:
+                json_file_abs = os.path.join(
+                    self._localization_dir_abs, f'{language}.json')
+                if os.path.isfile(json_file_abs):
+                    with open(json_file_abs, 'r') as file:
+                        translations = json.load(file)
+            ret = Localizer(translations)
+            self._instances[language] = ret
 
-        return Localizer(ret)
+        return ret

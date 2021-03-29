2c01def1799f098b629c776968dd8cd470187981
Christian Aistleitner <christian@quelltextlich.at>
2021-03-29 09:12:50 +0200

    frontend: input: Re-factor key command handler into command handler

--- frontend/javascript/input.js
+++ frontend/javascript/input.js
@@ -12,43 +12,46 @@ var keymap = {
   'd': 'toggle-developer-information',
 };
 
+var run_frontend_command = function(command) {
+    if (command.startsWith('cgi:')) {
+        callCgi(command.substring(4));
+    } else {
+        switch (command) {
+        case 'add-actor-random':
+            ScActorManager.addActorRandom();
+            break;
+        case 'fullscreen':
+            canvases = document.getElementsByTagName('canvas');
+            if (canvases.length) {
+                canvases[0].requestFullscreen();
+            }
+            break;
+        case 'toggle-developer-information':
+            DeveloperInformation.toggleVisibility();
+            break;
+        case 'toggle-frame-counter':
+            FrameCounter.toggleVisibility();
+            break;
+        case 'toggle-help':
+            // Phaser does not offer a keycode for question mark, so we trigger from
+            // outside.
+            HelpPage.toggleVisibility();
+            break;
+        case 'toggle-pause':
+            // We (un)pause outside of the game itself, as a paused game did not handle
+            // keydown events reliable and hence made it tricky to unpause.
+            PauseManager.toggle();
+            break;
+        default:
+            const msg = localize('Unknown frontend command "{command}"', {command: command});
+            MessageManager.addMessage(msg, 'failed');
+        }
+    }
+}
+
 document.addEventListener("keypress", function(e) {
     if (e.key in keymap) {
-        var command = keymap[e.key];
-        if (command.startsWith('cgi:')) {
-            callCgi(command.substring(4));
-        } else {
-            switch (command) {
-            case 'add-actor-random':
-                ScActorManager.addActorRandom();
-                break;
-            case 'fullscreen':
-                canvases = document.getElementsByTagName('canvas');
-                if (canvases.length) {
-                    canvases[0].requestFullscreen();
-                }
-                break;
-            case 'toggle-developer-information':
-                DeveloperInformation.toggleVisibility();
-                break;
-            case 'toggle-frame-counter':
-                FrameCounter.toggleVisibility();
-                break;
-            case 'toggle-help':
-                // Phaser does not offer a keycode for question mark, so we trigger from
-                // outside.
-                HelpPage.toggleVisibility();
-                break;
-            case 'toggle-pause':
-                // We (un)pause outside of the game itself, as a paused game did not handle
-                // keydown events reliable and hence made it tricky to unpause.
-                PauseManager.toggle();
-                break;
-            default:
-                const msg = localize('Unknown frontend command "{command}"', {command: command});
-                MessageManager.addMessage(msg, 'failed');
-            }
-        }
+        run_frontend_command(keymap[e.key]);
     }
 }, false);
 

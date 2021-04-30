e16606f799eb7af1cca17822f9dd7c9a256f3e87
Christian Aistleitner <christian@quelltextlich.at>
2021-04-30 14:25:21 +0200

    ScanariumError: Add private parameters
    Private parameters should carry debug information that is not intended
    for the public.

--- scanarium/ScanariumError.py
+++ scanarium/ScanariumError.py
@@ -4,11 +4,13 @@ from .MessageFormatter import MessageFormatter
 
 
 class ScanariumError(RuntimeError):
-    def __init__(self, code, template, parameters={}, *args, **kwargs):
+    def __init__(self, code, template, parameters={}, private_parameters={},
+                 *args, **kwargs):
         super(ScanariumError, self).__init__(*args, **kwargs)
         self.code = code
         self.template = template
         self.parameters = parameters
+        self.private_parameters = private_parameters
         self.message = MessageFormatter().format_message(
             self.template, self.parameters)
         self.uuid = uuid.uuid4()

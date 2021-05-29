e1966a6edfef029bf81fd6fdabbdf15e6ceba5d5
Christian Aistleitner <christian@quelltextlich.at>
2021-05-29 08:27:07 +0200

    photos: Add PhotoCastleRuin actor

---
+++ scenes/photos/actors/PhotoCastleRuin/PhotoCastleRuin.js
@@ -0,0 +1,8 @@
+class PhotoCastleRuin extends Photo {
+    constructor(x, y, flavor) {
+        const widthMm = 150; // mm
+        super(flavor, x, y, widthMm);
+    }
+}
+
+ScActorManager.registerActor(PhotoCastleRuin);

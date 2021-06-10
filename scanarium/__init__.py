ec6e3d9bcea1843c0b399fe22afd262257019512
Christian Aistleitner <christian@quelltextlich.at>
2021-06-10 14:12:00 +0200

    Python library: Allow to skip importing non-default dependencies
    This helps with re-using (parts of) the library in settings where for
    example OpenCV is not available.

--- scanarium/__init__.py
+++ scanarium/__init__.py
@@ -4,6 +4,13 @@
 
 # flake8: noqa
 
+def allow_non_default_dependencies():
+    import os
+    try:
+        return os.environ['SCANARIUM_SKIP_NON_DEFAULT_DEPENDENCIES'] != 'yes'
+    except KeyError:
+        return True
+
 from .Config import Config
 from .CommandLogger import CommandLogger
 from .Dumper import Dumper
@@ -14,7 +21,8 @@ from .Localizer import Localizer
 from .LocalizerFactory import LocalizerFactory
 from .Resetter import Resetter
 from .Result import Result
-from .Scanner import Scanner
+if allow_non_default_dependencies():
+    from .Scanner import Scanner
 from .Scanarium import Scanarium
 from .ScanariumError import ScanariumError
 from .Util import Util

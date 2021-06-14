750e2c951b15b5621f8dff9c84a2363b5ca6c67f
Christian Aistleitner <christian@quelltextlich.at>
2021-06-14 21:12:52 +0200

    frontend: url-parameters: Whitelist dash in parameter values and names
    Dashes are harmless, and are needed for passing uuids as parameters in
    follow-up commits.

--- frontend/javascript/url-parameters.js
+++ frontend/javascript/url-parameters.js
@@ -5,7 +5,7 @@
 function parseUrlParameters()
 {
     var parameters = new URLSearchParams("");
-    var allowed = /^[a-zA-Z0-9_]*$/;
+    var allowed = /^[a-zA-Z0-9_-]*$/;
     (new URLSearchParams(window.location.search.substring(1))).forEach((value, key) => {
         if (key.match(allowed) && value.match(allowed) && !parameters.has(key)) {
           parameters.set(key, value);

d9623b384c2eb673fed9efde1a8db6330951fe68
Christian Aistleitner <christian@quelltextlich.at>
2021-07-08 16:58:56 +0200

    photos: Add PlainPhoto9x13cm actor

---
+++ scenes/photos/actors/PlainPhoto9x13cm/PlainPhoto9x13cm.js
@@ -0,0 +1,12 @@
+// This file is part of Scanarium https://scanarium.com/ and licensed under the
+// GNU Affero General Public License v3.0 (See LICENSE.md)
+// SPDX-License-Identifier: AGPL-3.0-only
+
+class PlainPhoto9x13cm extends Photo {
+    constructor(x, y, flavor) {
+        const widthMm = 130; // mm
+        super(flavor, x, y, widthMm);
+    }
+}
+
+ScActorManager.registerActor(PlainPhoto9x13cm);
